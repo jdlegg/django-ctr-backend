de2d49ec4a3c2412d2e2ed1f4d8760a4d758a4ff
Jeffery Legg <jeffrey.d.legg@gmail.com>
2021-09-05 10:44:35 -0400

    test

--- ctf_backend/settings.py
+++ ctf_backend/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from datetime import timedelta
 import django_heroku
 
+# Testing this
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
